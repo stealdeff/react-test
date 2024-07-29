f51c321a42b9a39a58417af4a962939df587b8ba
arina <ukam.naletko@mail.ru>
2024-07-29 14:20:29 +0300

    rener

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+const ul1=(
+  <ul type="square">
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
+);
+const ul2=(
+  <ul type="square">
+    <li>1</li>
+    <li>2</li>
+    <li>3333</li>
+  </ul>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(ul1);
+setTimeout(
+  ()=>{root.render(ul2)},2000
+)
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
