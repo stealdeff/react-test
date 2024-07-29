04669b0094984dd4ac91f4a0efa12a233b921de3
arina <ukam.naletko@mail.ru>
2024-07-29 15:13:38 +0300

    props

--- src/index.js
+++ src/index.js
@@ -21,9 +21,31 @@ const ul2=(
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(ul1);
 setTimeout(
-  ()=>{root.render(ul2)},2000
+  ()=>{root.render(ul2)},8000
 )
 
+
+function Welcome(props) {
+  return <div>Welcome {props.name} {props.surename}</div>;
+}
+
+function App(props) {
+  const names = [
+    { name: "John", surename: "Doe" },
+    { name: "Billy", surename: "Bootcher" },
+    { name: "Anny", surename: "January" },
+  ];
+
+  return (
+    <div>
+      <Welcome name={names[0].name} surename={names[0].surename} />
+      <Welcome name={names[1].name} surename={names[1].surename} />
+      <Welcome name={names[2].name} surename={names[2].surename} />
+    </div>
+  );
+}
+
+root.render(<App />);
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 // root.render(
 //   <React.StrictMode>
