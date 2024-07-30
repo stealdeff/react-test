12743b44a0c45760987655142449fe34dc842499
arina <ukam.naletko@mail.ru>
2024-07-30 12:19:22 +0300

    create the receiving currency data file

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@
 import './App.css';
 import React from 'react';
 
-function App() {
-  return (
-    <div 
-    className="App"
-     style={{
-      color:'red',
-      backgroundColor:'black'
-    }}
-      > Babel training </div>
-)
-  }
+
+
+
+
+// function App() {
+//   return (
+//     <div 
+//     className="App"
+//      style={{
+//       color:'red',
+//       backgroundColor:'black'
+//     }}
+//       > Babel training </div>
+// )
+//   }
 
 
 export default App;
