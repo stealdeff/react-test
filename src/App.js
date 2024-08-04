dd77acca869f218b0298ae9ab04164aa49888a18
arina <ukam.naletko@mail.ru>
2024-08-04 15:52:52 +0300

    css

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import React from 'react';
-//import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-// import App from './App';
 import Home from './components/Home';
 import ExchangeRatesComponent from './components/CurrencyRatesByDate';
 import DinamicRatesComponent from './components/CurrencyRatesByPeriod';
 import Converter from './components/CurrencyConverter';
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-
+import './Project.css';
 const App = ()=>{
   return(
   <Router>
-    <div>
-    <nav>
+    <div class="container">
+    <input type="checkbox" id="toggle" checked />
+    <label class="button" for="toggle">
+    <nav class="nav">
       <ul>
         <li>
           <Link to="/components/Home">Home</Link>
         </li>
         <li>
-          <Link to="/CurrencyRatesByDate">Экран получения данных по валютам за определенный день</Link>
+          <Link to="/CurrencyRatesByDate">Получение данных за определенный день</Link>
         </li>
         <li>
-          <Link to="/CurrencyRatesByPeriod">Экран отображения курса любой валюты в динамике</Link>
+          <Link to="/CurrencyRatesByPeriod">Отображение курса любой валюты в динамике</Link>
         </li>
         <li>
-          <Link to="/CurrencyConverter">Экран конвертера валют</Link>
+          <Link to="/CurrencyConverter">Конвертер валют</Link>
         </li>
       </ul>
     </nav>
+    </label>
 
     <Routes>
       <Route path="/components/Home" element={<Home />} />
@@ -39,4 +39,5 @@ const App = ()=>{
   </Router>
 );
 };
+
 export default App;
